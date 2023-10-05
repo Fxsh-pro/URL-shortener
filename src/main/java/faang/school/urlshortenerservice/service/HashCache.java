5dabc8335b334d11a488572be0da51d659691593
Matvey <matveynikishaev@gmail.com>
2023-10-05 17:25:20 +0300

    Initial commit

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -0,0 +1,43 @@
+package faang.school.urlshortenerservice.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class HashCache {
+    @Value("${hash.cache.maxCapacity:1000}")
+    private Integer maxCapacity;
+    @Value("${hash.cache.border:0.2}")
+    private Double border;
+    private final HashGenerator hashGenerator;
+    private final AtomicBoolean fillingNow = new AtomicBoolean(false);
+    private Queue<String> hashQueue;
+
+    @PostConstruct
+    private void init() {
+        hashQueue = new ArrayBlockingQueue<>(maxCapacity);
+        hashQueue.addAll(hashGenerator.getHashes(maxCapacity));
+    }
+
+    @Retryable
+    public String getHash() {
+        if (hashQueue.size() < border * maxCapacity) {
+            if (fillingNow.compareAndExchange(false, true)) {
+                hashGenerator.getHashesAsync(maxCapacity)
+                        .thenAccept(hashQueue::addAll)
+                        .thenRun(() -> fillingNow.set(false));
+            }
+        }
+        return hashQueue.poll();
+    }
+}
